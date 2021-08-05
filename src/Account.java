1b2dc0ec8b4c2bc9de7a8c20f0ca490667c45fa1
manoj <manojs121@rediffmail.com>
2021-08-05 18:49:16 +0530

    050821

--- src/Account.java
+++ src/Account.java
@@ -6,6 +6,24 @@ public class Account {
 	private int balance;
 	private static int totalBalance;					 
 	
+	public static int add(int number1, int number2) {
+		return number1+number2;
+	}
+	
+	public static void info() {
+		System.out.println("Its A Banking Application...!");
+		System.out.println("Developed For Managing Accounts....!");
+		System.out.println("________________________________________________________");
+	}
+	//suppose we wish to compute the avg-balance of any two accounts.
+	
+	public static void showAvg(Account tmp1, Account tmp2) {
+		System.out.println("Avg of 2  :" +add(tmp1.balance,tmp2.balance)/2);
+	}
+	
+	
+	
+	
 	//non-static-version
 	public void showAccountDetails() {
 		System.out.println("Account Number : "+ano);
@@ -13,7 +31,7 @@ public class Account {
 		System.out.println("Balance   Rs.  : "+balance);
 		System.out.println("________________________________________________");
 	}
-	
+	/*
 	//static-version
 	public static void showAccountDetails(Account tmp) {
 		System.out.println("Account Number : "+tmp.ano);
@@ -21,8 +39,8 @@ public class Account {
 		System.out.println("Balance   Rs.  : "+tmp.balance);
 		System.out.println("________________________________________________");
 	}
-	
-	public void showTotalBalance() {
+	*/
+	public static void showTotalBalance() {
 		System.out.println("Total Balance : "+totalBalance);
 	}
 	public void setData(int p1, String p2, int p3) {
--- src/BankingApp.java
+++ src/BankingApp.java
@@ -3,6 +3,8 @@ public class BankingApp {
 
 	public static void main(String[] args) {
 		
+		Account.info();
+		
 		Account ac1=new Account();
 		Account ac2=new Account();
 		Account ac3=new Account();
@@ -11,16 +13,23 @@ public class BankingApp {
 		ac2.setData(112, "BBB", 20000);
 		ac3.setData(113, "CCC", 30000);
 		
-		Account.showAccountDetails(ac1);
-		Account.showAccountDetails(ac2);
-		Account.showAccountDetails(ac3);
+		//Account.showAccountDetails(ac1);
+		//Account.showAccountDetails(ac2);
+		//Account.showAccountDetails(ac3);
 		
-		//ac1.showAccountDetails();
-		//ac2.showAccountDetails();
-		//ac3.showAccountDetails();
+		ac1.showAccountDetails();
+		ac2.showAccountDetails();
+		ac3.showAccountDetails();
 		
-	
-
+		Account.showAvg(ac1, ac2);
+		Account.showAvg(ac2, ac3);
+		
+		//ac1.showAvg(ac2);
+		//ac2.showAvg(ac3);
+		//ac1.showAvg(ac3);
+		
+		Account.showTotalBalance();
+		
 	}
 
 }
