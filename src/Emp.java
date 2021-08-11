43092a2fa0b3eb125d86ca3874e1b3871b219408
manoj <manojs121@rediffmail.com>
2021-08-11 18:58:29 +0530

    110821

--- src/Emp.java
+++ src/Emp.java
@@ -3,13 +3,23 @@ public class Emp {
 	private int eno;
 	private String name;
 	private int sal;
-	
+
+	public void m1() {
+		
+	}
+	public Emp() {
+		m1();
+	}
+	public Emp(int p, String q) {
+		this();
+		eno=p;
+		name=q;
+	}
 	public Emp(int a, String b, int c) {
-		eno=a; name=b; sal=c;
+		this(a,b);	//2 arg constructor call
+		sal=c;
 	}
-	public void setData(int a, String b, int c) {
-		eno=a; name=b; sal=c;
-	}
+	
 	public void info() {
 		System.out.println("Emp Code : "+eno);
 		System.out.println("Emp Name : "+name);
@@ -18,12 +28,8 @@ public class Emp {
 	}
 	
 	public static void main(String[] args) {
-		Emp e1=new Emp(111,"AAA",10000);
-		Emp e2=new Emp(112,"BBB",20000);
-		//e1.setData(111, "AAA", 10000);
-		//e2.setData(112, "BBB", 20000);
-		e1.info();
-		e2.info();
+		Emp e=new Emp(111,"AAA",10000);
+		e.info();
 	}
 
 }
